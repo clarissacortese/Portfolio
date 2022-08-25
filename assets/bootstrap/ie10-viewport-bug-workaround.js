eb51f76ef648a507b209b963fc24983d48ede27b
clarissacortese <clarissacortese.info@gmail.com>
2022-08-25 17:22:12 +0200

    backlashes

--- assets/bootstrap/ie10-viewport-bug-workaround.js
+++ assets/bootstrap/ie10-viewport-bug-workaround.js
@@ -10,7 +10,7 @@
 (function () {
   'use strict';
 
-  if (navigator.userAgent.match(/IEMobile\/10\.0/)) {
+  if (navigator.userAgent.match(/IEMobile//10/.0/)) {
     var msViewportStyle = document.createElement('style')
     msViewportStyle.appendChild(
       document.createTextNode(
